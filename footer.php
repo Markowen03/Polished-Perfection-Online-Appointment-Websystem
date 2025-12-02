eef40e2c40e8fdca905fb47d4c68ac6aee8e6f38
Owen Dev <147114510+Markowen03@users.noreply.github.com>
2025-12-02 22:47:37 +0800

    I add the footer

---
+++ footer.php
@@ -0,0 +1,31 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>Quick links</h3>
+            <a href="home.php">Home</a>
+            <a href="services.php">Services</a>
+            <a href="staff.php">Staff</a>
+            <a href="appointment.php">Appointment</a>
+        </div>
+
+        <div class="box">
+            <h3>Contact info</h3>
+            <p> <i class="fas fa-phone"></i> 0908-449-4826 </p>
+            <p> <i class="fas fa-envelope"></i> polishedperfection@gmail.com </p>
+            <p> <i class="fas fa-map-marker-alt"></i> Puerto Princesa, Palawan</p>
+        </div>
+
+        <div class="box">
+            <h3>Follow us</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>Twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+        </div>
+
+    </div>
+
+    <div class="credit">&copy; Fullbright College <?php echo date('Y'); ?></div>
+
+</section>
